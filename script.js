e562a3affaf5e6a457f11478bd6f9c0407695023
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-10 15:24:14 +0100

    Change the DOM with JS to show the results

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 let max = null;
 let min = null;
 
-let entered = parseInt(prompt("Saisir une valeur : "));
 
-max = entered;
-min = entered;
-
-console.log("max : " + max);
-console.log("min : " + min);
-
-
-while(true)
-{
+function addANewNumber() {
     let valueEntered = prompt("Saisir une valeur : ");
-
-    if(valueEntered === "quit")
-        break;
 
     entered = parseInt(valueEntered);
 
-    if(max < entered) {
+    if(max < entered || max === null) {
         max = entered;
     }
 
-    if(min > entered) {
+    if(min > entered || min === null) {
         min = entered;
     }
 
-    console.log("max : " + max);
-    console.log("min : " + min);
+    const maxValue = document.getElementById("maxValue");
+    maxValue.innerHTML = max;
+    maxValue.style.color = "red";
+
+    const minValue = document.getElementById("minValue");
+    minValue.innerHTML = min;
+    minValue.style.fontFamily = "Arial";
+
+    const elements = document.getElementsByClassName("label");
+    for (const element of elements) {
+        element.classList.toggle("english");
+    }
 }
 
-
-console.log("max final : " + max);
-console.log("min final : " + min);
+const btn = document.getElementById("btnAddANumber");
+btn.addEventListener("click", addANewNumber);
